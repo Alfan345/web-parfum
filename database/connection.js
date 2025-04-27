bdb8a651d370b460dfe56715839c05379f1c69bc
Alfanah Muhson Husain Nugroho <alfanahmuhson@gmail.com>
2025-04-27 15:11:32 +0000

    change db

--- database/connection.js
+++ database/connection.js
@@ -3,10 +3,10 @@ const knex = require("knex");
 const db = knex({
   client: "mysql2",
   connection: {
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
+    host: process.env.DB_HOST || "webparfum-server.mysql.database.azure.com",
+    user: process.env.DB_USER || "czevyehtxx",
     password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "parfum_recommendation",
+    database: process.env.DB_NAME || "webparfum-database",
   },
   pool: {
     min: 2,
