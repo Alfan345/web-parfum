709d2aa46d87f66bc873c5e738f180b6c72198f5
Alfanah Muhson Husain Nugroho <alfanahmuhson@gmail.com>
2025-04-27 16:48:14 +0000

    cek error

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const db = require("../database/connection");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 class User {
   static async create({ name, email, password }) {
